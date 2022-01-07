4011f1e862cf35a7ae42e45efc0c070090d7642f
Nathaniel Pendy <nathanielp2402@gmail.com>
2022-01-07 11:47:33 -0600

    THREE

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,76 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 window.onload = () => {
-    
-}
+  let camera, controls, scene, renderer;
+
+  const init = () => {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xcccccc);
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(100, 50, 0);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.listenToKeyEvents(window);
+
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 5;
+    controls.maxDistance = 15;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+    const holeGeometry = new THREE.SphereGeometry();
+    const holeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+    const holeMesh = new THREE.Mesh(holeGeometry, holeMaterial);
+    scene.add(holeMesh);
+
+    const geometry = new THREE.RingGeometry(3, 5, 64);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const dirLight1 = new THREE.DirectionalLight(0xffffff);
+    dirLight1.position.set(1, 1, 1);
+    scene.add(dirLight1);
+
+    const dirLight2 = new THREE.DirectionalLight(0x002288);
+    dirLight2.position.set(-1, -1, -1);
+    scene.add(dirLight2);
+
+    const ambientLight = new THREE.AmbientLight(0x222222);
+    scene.add(ambientLight);
+
+    window.addEventListener('resize', onWindowResize);
+  }
+
+  const onWindowResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+  }
+
+  const render = () => {
+    renderer.render(scene, camera);
+  }
+
+  init();
+  animate();
+};
